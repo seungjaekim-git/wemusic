3822f4026887a59ed2ceebf1ae394251a6a8382a
seungjaekim-git <69844464+seungjaekim-git@users.noreply.github.com>
2022-02-11 11:55:26 +0900

    초기세팅

---
+++ src/main/java/com/scn/wemusic/WemusicApplication.java
@@ -0,0 +1,13 @@
+package com.scn.wemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scn/wemusic/WemusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scn.wemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
